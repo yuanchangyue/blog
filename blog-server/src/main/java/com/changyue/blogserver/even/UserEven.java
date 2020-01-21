4b55ea02a304bf8a15a9ef8120565a9765f36d10
fuhao <524686658@qq.com>
2020-01-21 08:33:25 +0800

    tag

--- blog-server/src/main/java/com/changyue/blogserver/even/UserEven.java
+++ blog-server/src/main/java/com/changyue/blogserver/even/UserEven.java
@@ -1,10 +1,15 @@
 package com.changyue.blogserver.even;
 
+import org.springframework.context.ApplicationEvent;
+
 /**
  * @program: blog-server
  * @description:
  * @author: ChangYue
  * @create: 2020-01-20 17:40
  */
-public class UserEven {
+public class UserEven extends ApplicationEvent {
+    public UserEven(Object source) {
+        super(source);
+    }
 }
